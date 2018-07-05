7aaae31cb23cdba088c4155c8bcb36a3c128236a
ArmandoHernandez14 <Xboxoneknight43@gmail.com>
2018-07-05 16:33:55 -0700

    Day 1

--- app.js
+++ app.js
@@ -1,19 +1,37 @@
-var express = require('express');
+//If this doesn't work, then type in var in front of
+//bodyParser, path,expressValidator,and mongoose
+var express = require('express'),
 //const mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var path = require('path');
-var expressValidator = require('express-validator');
-// connect to the database
-// mongoose.connect('mongodb://localhost/testaroo');
-// var url = 'mongodb://localhost:3000/test';
-
+ bodyParser = require('body-parser'),
+ path = require('path'),
+ expressValidator = require('express-validator'),
 // mongoose.connection.once('open',function(){
 //   console.log('Connection has been made, now make fireworks...');
 // }).on('error',function(error){
 //   console.log('Connection error');
 // });
+mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/test');
 
 var app = express();
+
+var nameSchema = new mongoose.Schema({
+  firstName:String,
+  lastname:String,
+  Email:String,
+  Message:String
+});
+//The database that your sending the data to is called "User",
+//not because of the variable. But because of the "User" in parenthesis
+var User = mongoose.model("User",nameSchema);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended:true
+}));
+
 /*
 var logger = function(req,res,next){
   console.log('Loggin...');
@@ -25,23 +43,23 @@ app.use(logger);*/
 //View Engine
 //======================================
 var fs = require('fs');
-var myCss = {
-  style : fs.readFileSync('views/style.css','utf8')
-};
+ var myCss = {
+  style : fs.readFileSync('public/style.css','utf8')
+ };
+ //Displays our HTML, and CSS files
 app.get('/', function(req,res){
   res.render('index.ejs',{
     myCss: myCss
   });
 });
 
+//app.post("/addclient",function(req,res){
+//  var body = req.body.firstName;
+//});
+
 app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname,'views'));
 //app.use(express.static(__dirname + '/public'));app.use(express.static(__dirname + '/public'));<link href="/css/style.css" rel="stylesheet" type="text/css">
-
-//Body Parser Middleware
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
 
 //Express Validator Middleware
 app.use(expressValidator({
@@ -49,7 +67,6 @@ app.use(expressValidator({
     var namespace = param.split('.')
     ,root = namespace.shift(),
     formParam = root;
-
     while(namespace.length){
       formParam += '[' + namespace.shift() + ']';
     }
@@ -75,35 +92,36 @@ app.use(function(req,res,next){
 //Routes
 //======================================
 
-
-app.post('/addclient', function(req,res){
-  client.create({
-  // res.render('index',{
-  //   title:'Customers',
-  //   users: users
-  // });
-});
-})
-app.post('/users/add', function(req,res){
-req.checkBody('first_name','First Name is Required').notEmpty();
-req.checkBody('last_name','Last Name is Required').notEmpty();
-req.checkBody('email','Email is Required').notEmpty();
-
-var errors = req.validationErrors();
-if(errors){
- res.render('index',{
-   title:'Customers',
-   users:users,
-   errors:errors
- });
-}else{
-  var newUser={
-    first_name: req.body.first_name,
-    last_name: req.body.last_name,
-    email:req.body.email
-  }
-  console.log('Success');
-}
+// app.post('/users/add', function(req,res){
+// req.checkBody('first_name','First Name is Required').notEmpty();
+// req.checkBody('last_name','Last Name is Required').notEmpty();
+// req.checkBody('email','Email is Required').notEmpty();
+//
+// var errors = req.validationErrors();
+// if(errors){
+//  res.render('index',{
+//    title:'Customers',
+//    users:users,
+//    errors:errors
+//  });
+// }else{
+//   var newUser={
+//     first_name: req.body.first_name,
+//     last_name: req.body.last_name,
+//     email:req.body.email
+//   }
+//   console.log('Success');
+// }
+// });
+app.post("/addname",(req,res) =>{
+  var myData = new User(req.body);
+  myData.save()
+  .then(item => {
+    res.redirect("/");
+  })
+  .catch(err => {
+    res.status(400).send("Unable to save to the database");
+  });
 });
 
 app.listen(3000,function(){
